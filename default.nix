8fd03fd973c50279c79732cef14018b8ddbe6200
Phil R <git@phrogg.de>
2023-04-20 14:45:48 +0200

    update

--- default.nix
+++ default.nix
@@ -1,35 +1,35 @@
 { pkgs ? import <nixpkgs> { }
 , lib ? import <nixpkgs/lib>
+, autoPatchelfHook
 }:
 pkgs.stdenv.mkDerivation rec {
   pname = "skfde";
   version = "0.1.1";
 
-  src = [ ./. ];
+  #src = [ ./. ];
 
-  #cargoDeps = pkgs.rustPlatform.importCargoLock {
-  #  lockFile = ./Cargo.lock;
-  #  outputHashes = {
-  #    "nix-data-0.0.2" = "sha256-8j3j+Bk90eBBqQkguwU0tTtMU7Zecjhr1SbW5hnptFY=";
-  #  };
-  #};
+  src = pkgs.fetchgit {
+    url = "https://github.com/phrogg/solokey-full-disk-encryption";
+    rev = "e60d41d0fbb6f26fd5f702e731516affc0bbfcd2";
+    sha256 = "sha256-mzNERqOPxjojVR1jGe+1luPwNN5QrZUsxESirpR8cuM=";
+  };
 
-  nativeBuildInputs = with pkgs; [
-    git
-    gnumake
+
+  tiveBuildInputs = [
+    autoPatchelfHook
   ];
 
   buildInputs = with pkgs; [
     #glib
   ];
 
-  sourceRoot = ".";
+  # sourceRoot = ".";
 
 
   installPhase = ''
     # install -m755 -D studio-link-standalone-v${version} $out/bin/studio-link
-    install -v -b -Dm644 src/skfde.conf "$(DESTDIR)/etc/skfde.conf"
-
+    install -v -b -Dm644 src/skfde.conf "$out/etc/skfde.conf"
+    print $src
     install -Dm644 src/hooks/skfde "$out/lib/initcpio/hooks/skfde"
     install -Dm644 src/install/skfde "$out/lib/initcpio/install/skfde"
     install -Dm755 src/skfde-enroll "$out/bin/skfde-enroll"
@@ -41,6 +41,6 @@ pkgs.stdenv.mkDerivation rec {
   '';
 
   postInstall = ''
-    wrapProgram $out/bin/skfde --prefix PATH : '${lib.makeBinPath [ pkgs.pandoc ]}'
+    # wrapProgram $out/bin/skfde --prefix PATH : '${lib.makeBinPath [ pkgs.pandoc ]}'
   '';
 }
