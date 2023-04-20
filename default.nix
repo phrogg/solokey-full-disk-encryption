e60d41d0fbb6f26fd5f702e731516affc0bbfcd2
Phil R <git@phrogg.de>
2023-04-20 13:06:26 +0200

    update

--- default.nix
+++ default.nix
@@ -23,6 +23,23 @@ pkgs.stdenv.mkDerivation rec {
     #glib
   ];
 
+  sourceRoot = ".";
+
+
+  installPhase = ''
+    # install -m755 -D studio-link-standalone-v${version} $out/bin/studio-link
+    install -v -b -Dm644 src/skfde.conf "$(DESTDIR)/etc/skfde.conf"
+
+    install -Dm644 src/hooks/skfde "$out/lib/initcpio/hooks/skfde"
+    install -Dm644 src/install/skfde "$out/lib/initcpio/install/skfde"
+    install -Dm755 src/skfde-enroll "$out/bin/skfde-enroll"
+    install -Dm755 src/skfde-format "$out/bin/skfde-format"
+    install -Dm755 src/skfde-open "$out/bin/skfde-open"
+    install -Dm755 src/skfde-load "$out/bin/skfde-load"
+    install -Dm755 src/skfde-cred "$out/bin/skfde-cred"
+    #install -Dm644 README.md "$out/share/doc/skfde/README.md"
+  '';
+
   postInstall = ''
     wrapProgram $out/bin/skfde --prefix PATH : '${lib.makeBinPath [ pkgs.pandoc ]}'
   '';
